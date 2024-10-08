57eb6d7b93063b9367c574c5cca89535c058aae2
mo-policy <policy@mobileownership.org>
2024-10-08 15:05:18 -0700

    app.js takes arg for policy json file to load and run.

--- src/PolicyTS/app.js
+++ src/PolicyTS/app.js
@@ -1,4 +1,16 @@
 "use strict";
 // Copyright (c) Mobile Ownership, mobileownership.org.  All Rights Reserved.  See LICENSE.txt in the project root for license information.
-console.log('Hello world');
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const machine_1 = require("./machine");
+const term_1 = require("./term");
+if (process.argv.length === 3) {
+    let plfile = process.argv[2];
+    const js = (0, fs_1.readFileSync)(plfile).toString();
+    let term = JSON.parse(js);
+    const m = new machine_1.Machine(term);
+    const r = (0, term_1.rewriteTerm)(m);
+    const rjs = JSON.stringify(r.term);
+    process.stdout.write(rjs);
+}
 //# sourceMappingURL=app.js.map
--- src/PolicyTS/app.ts
+++ src/PolicyTS/app.ts
@@ -1,4 +1,16 @@
 // Copyright (c) Mobile Ownership, mobileownership.org.  All Rights Reserved.  See LICENSE.txt in the project root for license information.
 
-console.log('Hello world');
+import { readFileSync } from "fs";
+import { Machine } from "./machine";
+import { rewriteTerm } from "./term";
 
+if (process.argv.length === 3) {
+    let plfile = process.argv[2];
+    const js = readFileSync(plfile).toString();
+    let term = JSON.parse(js);
+
+    const m = new Machine(term);
+    const r = rewriteTerm(m);
+    const rjs = JSON.stringify(r.term);
+    process.stdout.write(rjs);
+}
